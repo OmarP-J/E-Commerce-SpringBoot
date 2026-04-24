18a0ec0cecc8aaef1141cf9b0cac716da5f0f5ff
OmarP-J <jopjimenez21@gmail.com>
2026-04-24 17:30:24 -0400

    Finish with the Creation Signup Page, API Call and Auth Service

--- EcommerceWeb/src/app/app.module.ts
+++ EcommerceWeb/src/app/app.module.ts
@@ -5,16 +5,26 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { DemoAngularMaterialModule } from "./DemoAngularMaterialModule";
+import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        LoginComponent,
+        SignupComponent
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
         AppRoutingModule,
-        DemoAngularMaterialModule
+        DemoAngularMaterialModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+
     ],
     providers: [],
     bootstrap: [AppComponent]
