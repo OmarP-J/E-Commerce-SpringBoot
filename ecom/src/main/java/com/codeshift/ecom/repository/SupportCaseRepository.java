c4ecf993cb8cc9ef91eb7f8d83d682d4f4ac3ee2
Jaroly Omar Polanco Jimenez <jopjimenez21@gmail.com>
2026-09-11 09:25:15 -0400

    Add EntityGraph to SupportCaseRepository methods
    Added EntityGraph annotations to optimize queries for customer and handledBy relationships.

--- ecom/src/main/java/com/codeshift/ecom/repository/SupportCaseRepository.java
+++ ecom/src/main/java/com/codeshift/ecom/repository/SupportCaseRepository.java
@@ -1,12 +1,15 @@
 package com.codeshift.ecom.repository;
 
 import com.codeshift.ecom.model.SupportCase;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface SupportCaseRepository extends JpaRepository<SupportCase, Long> {
+    @EntityGraph(attributePaths = { "customer", "handledBy" })
     List<SupportCase> findByCustomerIdOrderByCreatedAtDesc(Long customerId);
 
+    @EntityGraph(attributePaths = { "customer", "handledBy" })
     List<SupportCase> findAllByOrderByUpdatedAtDesc();
 
     List<SupportCase> findByOrderId(Long orderId);
