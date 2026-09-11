03653092993063aa9e0390a4c5ca7bf613ca3031
Jaroly Omar Polanco Jimenez <jopjimenez21@gmail.com>
2026-09-11 09:24:31 -0400

    Add EntityGraph to InventoryMovementRepository

--- ecom/src/main/java/com/codeshift/ecom/repository/InventoryMovementRepository.java
+++ ecom/src/main/java/com/codeshift/ecom/repository/InventoryMovementRepository.java
@@ -1,9 +1,11 @@
 package com.codeshift.ecom.repository;
 
 import com.codeshift.ecom.model.InventoryMovement;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface InventoryMovementRepository extends JpaRepository<InventoryMovement, Long> {
+    @EntityGraph(attributePaths = { "product", "performedBy" })
     List<InventoryMovement> findTop100ByOrderByCreatedAtDesc();
 }
